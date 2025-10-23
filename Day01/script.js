61423e603defa5333e8bf81dce55c57e4a23e684
Onotina Imoudu <77248527+Onotina@users.noreply.github.com>
2025-10-23 04:49:21 +0100

    Added and Completed The Day 01 of the challenge

---
+++ Day01/script.js
@@ -0,0 +1,14 @@
+// console.log("Hello from Node.js");
+
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript");
+document.write("Check the browser console for a message");
+
+// Adding HTML Elements
+document.write("<h1>Written From Document.Write()</h1>");
+document.write('<div class="container">', 
+                    '<h2>This is the heading</h2>',
+                    '<p>This is a paragraph</p>', 
+                    '<br />',
+                '</div>'
+    );
---
+++ Day01/test.js
@@ -0,0 +1,3 @@
+console.log("I am a test script");
+
+document.getElementById("uniqueID").innerText = "I am a unique ID";
